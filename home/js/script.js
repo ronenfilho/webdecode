2f10b3c77bfa93eb1b16d3bd06a3f227146591e3
ronen.blpit <ronen.filho@blpit.com.br>
2019-12-28 15:59:27 -0300

    reiniciar

--- home/js/script.js
+++
@@ -1,8 +0,0 @@
-
-
-        $(function () {
-            $('.go-down').click(function () {
-                $('html,body').animate({scrollTop: $('#after-hero').offset().top}, 500);
-            });
-        });
-    
